237914bb151dfed6054b61dd76dd645fadf85f3b
sglowa <sglowa@saic.edu>
2020-08-05 01:18:32 +0200

    changed ice servers

--- index.js
+++ index.js
@@ -15,16 +15,20 @@ navigator.getUserMedia({video:true,audio:true},(myStream)=>{
 		initiator: location.hash === '#init',
 		trickle: false,
 		config: {
-			iceServers: [{
-				"urls": "stun:stun.l.google.com:19302"
-			},
-		// public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
-		// set your own servers here
-			{
-				url: 'turn:192.158.29.39:3478?transport=udp',
-				credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-				username: '28224511:1379330808'
-			}]
+			iceServers: [
+				{
+					"urls": "stun:numb.viagenie.ca",
+      				"username": "s9lowacki@gmail.com", 
+      				"credential": "testingtesting"
+				},
+			// public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
+			// set your own servers here
+				{
+					"urls": "turn:numb.viagenie.ca",
+					"username": "s9lowacki@gmail.com",
+					"credential": "testingtesting"
+				}
+			]
 		    },
 		stream: myStream
 	});	
